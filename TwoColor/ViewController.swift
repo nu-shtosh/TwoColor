e2e2d0d70c3fc072ba726cd7686a33b2197a28b1
Ilia <dubenskiiilia@gmail.com>
2022-09-12 23:59:30 +0300

    Initial Commit

---
+++ TwoColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoColor
+//
+//  Created by Илья Дубенский on 12.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
